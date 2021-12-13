569a22efa721c2f5645dd29435d3ee7cdbfc85b7
Theo Sandell <theo01sandell@gmail.com>
2021-12-13 10:28:34 +0100

    Updated to PSR12

--- routes/console.php
+++ routes/console.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command(
-    'inspire', function () {
+    'inspire',
+    function () {
         $this->comment(Inspiring::quote());
     }
 )->purpose('Display an inspiring quote');
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get(
-    '/', function () {
+    '/',
+    function () {
         return view('welcome');
     }
 );
