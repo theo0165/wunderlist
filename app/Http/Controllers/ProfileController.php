98a9b7cdc27f1d0d5e268a7a955f04e0e37f26c4
Theo Sandell <theo01sandell@gmail.com>
2021-12-14 01:10:47 +0100

    Created profile route

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show()
+    {
+        //dd(Auth::user()->name);
+        return view('profile.show', [
+            'user' => Auth::user()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'show'])->name('profile');
