9b5b8c187cde19e421582a7941294aabc4e10254
Theo Sandell <theo01sandell@gmail.com>
2021-12-14 20:07:13 +0100

    Created task and task list migrations

---
+++ database/migrations/2021_12_14_175420_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('deadline')->nullable();
+            $table->unsignedBigInteger('list_id');
+            $table->timestamps();
+
+            $table->index('list_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
