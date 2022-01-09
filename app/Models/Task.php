335cd357fa182a1f8b86a2b27775bcf462c09729
Theo Sandell <theo01sandell@gmail.com>
2022-01-09 21:08:24 +0100

    Casting completed as boolean in Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,6 +13,10 @@ class Task extends Model
 {
     use HasFactory;
 
+    public $casts = [
+        'completed' => 'boolean'
+    ];
+
     public $fillable = [
         'title',
         'description',
